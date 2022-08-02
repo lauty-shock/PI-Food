1c17c1ed0caa254775992e42de1c6b3639847fd5
lauty-shock <87452876+lauty-shock@users.noreply.github.com>
2022-08-01 21:30:31 -0300

    Ready

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -2,74 +2,51 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
-import "./Detail.css";
+import s from "./Detail.module.css";
 
 export default function Detail() {
   const details = useSelector((state) => state.detail);
+
   return (
-    // <div className="detail">
-    //   <div className="detail-div">
-    //     <div className="detail-div-img">
-    //       <img className="detail-img" src={details.image} alt="Food image" />
-    //     </div>
+    <div className={s.detail}>
+      <Link to="/home">
+        <button className={s.detailBtn}>
+          <h6>BACK</h6>
+        </button>
+      </Link>
 
-    //     <div className="">
-    //       <h1 className="detail-title">{details.title}</h1>
-    //     </div>
-    //   </div>
-    // </div>
-    <>
-      <div className="detail">
-        <div className="detail-div">
-          <div className="detail-div-btn">
-            <div className="btn">
-              <Link to="/home">
-                <button className="detail-btn">
-                  <h6 className="detail-h6">BACK</h6>
-                </button>
-              </Link>
+      <div className={s.detailDiv}>
+        <div className={s.detailDivTop}>
+          <img src={details.image} alt="recipe" className={s.detailImage} />
+          <div className={s.detailDivTopRigth}>
+            <h1 className={s.detailTitle}>{details.title}</h1>
+            <div className={s.detailHealthScore}>
+              <h3 className={s.detailTitle}>Health Score:</h3>
+              <p>{details.healthScore}</p>
             </div>
-            <h1>{details.title}</h1>
-            <div className="data">
-              <div className="detail-data">
-                <p className="detail-p">
-                  <h3 className="detail-h3">Dish types: </h3>
-                  {details.dishTypes}
-                </p>
-              </div>
-              <div className="detail-data">
-                <p className="detail-p">
-                  <h3 className="detail-h3">Diets types: </h3>
-                  {details.diets}
-                </p>
-              </div>
-              <div className="detail-data">
-                <p className="detail-p">
-                  <h3 className="detail-h3">Health Score: </h3>
-                  {details.healthScore}
-                </p>
-              </div>
-            </div>
+            <h3 className={s.detailTitle}>Dish Types:</h3>
+            {details.dishTypes}
+            <h3 className={s.detailTitle}>Diet Types:</h3>
+            {details.diets &&
+              details.diets.map((e, i) => {
+                return <li key={i}>{e},</li>;
+              })}
           </div>
-
-          <div className="detail-div-img">
-            <img className="detail-img" src={details.image} alt="Food image" />
-          </div>
-
-          {details.summary && (
-            <p className="detail-p">
-              <h3 className="detail-h3">Summary: </h3>
-              {details.summary.replace(/<\/?[^>]+(>|$)/g, " ")}
-            </p>
-          )}
-          {details.instructions && (
-            <p className="detail-p">
-              <h3 className="detail-h3">Instructions: </h3>
-              {details.instructions.replace(/<\/?[^>]+(>|$)/g, " ")}
-            </p>
-          )}
         </div>
+
+        <h3 className={s.detailTitleLow}>Summary:</h3>
+        {details.summary && (
+          <p className={s.detailP}>
+            {details.summary.replace(/<\/?[^>]+(>|$)/g, " ")}
+          </p>
+        )}
+        <h3 className={s.detailTitleLow}>Instructions:</h3>
+        {details.instructions && (
+          <p className={s.detailP}>
+            {details.instructions.replace(/<\/?[^>]+(>|$)/g, " ")}
+          </p>
+        )}
       </div>
-    </>
+    </div>
   );
 }
