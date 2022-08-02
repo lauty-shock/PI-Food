f53ff4716cee029d0263031c6f52d75975beae2e
lauty-shock <87452876+lauty-shock@users.noreply.github.com>
2022-08-01 21:52:39 -0300

    Ready

--- client/src/components/Create/Create.jsx
+++ client/src/components/Create/Create.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 import { createRecipe } from "../../redux/actions";
 
 import "./Create.css";
