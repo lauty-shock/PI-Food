e2fd6cbce3af693ae22b51ae9be06f0d00070243
lauty-shock <87452876+lauty-shock@users.noreply.github.com>
2022-10-29 00:41:14 -0300

    New style of details

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -8,46 +8,71 @@ export default function Detail() {
   const details = useSelector((state) => state.detail);
 
   return (
-    <div className={css.detail}>
+    <div className={css.container}>
       <Link className={css.detailBtn} to="/home">
         <h6>BACK</h6>
       </Link>
 
-      <div className={css.detailDiv}>
-        <div className={css.detailDivTop}>
-          <img src={details.image} alt="recipe" className={css.detailImage} />
-          <div className={css.detailDivTopRigth}>
-            <h1 className={css.detailTitle}>{details.title}</h1>
-            <div className={css.detailHealthScore}>
-              <h3 className={css.detailTitle}>Health Score:</h3>
-              <p>{details.healthScore}</p>
+      <div className={css.detail}>
+        <div className={css.columLeft}>
+          <h1 className={css.title}>{details.title}</h1>
+          <section className={css.info}>
+            <div className={css.data}>
+              <h3 className={css.typeTitle}>Dish Types</h3>
+              <ul className={css.containerType}>
+                {Array.isArray(details.dishTypes) ? (
+                  details.dishTypes.map((e, i) => {
+                    return (
+                      <li className={css.type} key={i}>
+                        {e}
+                      </li>
+                    );
+                  })
+                ) : (
+                  <li className={css.type}>{details.dishTypes}</li>
+                )}
+              </ul>
             </div>
-            <h3 className={css.detailTitle}>Dish Types:</h3>
-            {details.dishTypes}
-            <h3 className={css.detailTitle}>Diet Types:</h3>
-            {details.diets &&
-              details.diets.map((e, i) => {
-                return <li key={i}>{e}</li>;
-              })}
+            <div className={css.data}>
+              <h3 className={css.typeTitle}>Diet Types</h3>
+              <ul className={css.containerType}>
+                {details.diets &&
+                  details.diets.map((e, i) => {
+                    return (
+                      <li className={css.type} key={i}>
+                        {e}
+                      </li>
+                    );
+                  })}
+              </ul>
+            </div>
+
+            <div className={css.data}>
+              <h3 className={css.typeTitle}>Health Score</h3>
+              <span className={css.healthScore}>{details.healthScore}</span>
+            </div>
+          </section>
+
+          <h3 className={css.detailTitleLow}>Instructions:</h3>
+          <div className={css.divData}>
+            {details.instructions && (
+              <p className={css.detailP}>
+                {details.instructions.replace(/<\/?[^>]+(>|$)/g, " ")}
+              </p>
+            )}
           </div>
         </div>
 
-        <h3 className={css.detailTitleLow}>Summary:</h3>
-        <div className={css.divData}>
-          {details.summary && (
-            <p className={css.detailP}>
-              {details.summary.replace(/<\/?[^>]+(>|$)/g, " ")}
-            </p>
-          )}
-        </div>
-
-        <h3 className={css.detailTitleLow}>Instructions:</h3>
-        <div className={css.divData}>
-          {details.instructions && (
-            <p className={css.detailP}>
-              {details.instructions.replace(/<\/?[^>]+(>|$)/g, " ")}
-            </p>
-          )}
+        <div className={css.columRigth}>
+          <img src={details.image} alt="recipe" className={css.detailImage} />
+          <h3 className={css.detailTitleLow}>Summary:</h3>
+          <div className={css.divData}>
+            {details.summary && (
+              <p className={css.detailP}>
+                {details.summary.replace(/<\/?[^>]+(>|$)/g, " ")}
+              </p>
+            )}
+          </div>
         </div>
       </div>
     </div>
